7a880f0c98bbb35c68f641ae5d631d6228584c94
DenisT90 <66994655+DenisT90@users.noreply.github.com>
2022-05-08 11:48:38 +0500

    add text lab number before line

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 #region lab1
 var rgbType = new ImmutableRgbType(1,2,3);
 
+Console.WriteLine("Lab 1:");
 Console.WriteLine($"Red - {rgbType.Red}");
 Console.WriteLine($"Green - {rgbType.Green}");
 Console.WriteLine($"Blue - {rgbType.Blue}");
@@ -10,6 +11,8 @@ Console.WriteLine($"Blue - {rgbType.Blue}");
 #endregion lab1
 
 #region lab2
+Console.WriteLine("");
+Console.WriteLine("Lab 2:");
 var baseValue = new BaseOverridden();
 Console.WriteLine(baseValue.GetInfo());
 var baseOverlap = new BaseOverlap();
